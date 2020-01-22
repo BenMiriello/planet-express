5cc5ceb5a72980b00eac33c3abf4dd9d4da229a1
BenMiriello <benmiriello@gmail.com>
2020-01-21 22:00:09 -0500

    made planet divs click to open buy modal with destination already selected to the clicked planet

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const planetsUrl = `http://localhost:3000/planets`
 // DOM selector
 const planetsCollection = document.querySelector('#planet-collection')
 const body = document.querySelector('body')
-
+const planetDivsArray = []
 let planetsArray = []
+
 // First fetch, to display all planets in there respective boxes
 fetch(planetsUrl)
 .then(r => r.json())
@@ -15,112 +16,93 @@ planets.forEach((planetObj) => {
   })
 createBuyModalBox(planetsArray)
 })
-// console.log(planetsArray)
-// creating planet boxes elements
+
 function turnEachPlanetsToHTML(planetObj){
     let div = document.createElement('div')
     div.className = "planet-box"
+    div.dataset.id = `d${planetObj.id}`
 
     let img = document.createElement('img')
     img.className = "planet-image"
     img.src = planetObj.image
     img.alt = planetObj.name
+    img.dataset.id = `i${planetObj.id}`
     
     let p = document.createElement('p')
     p.innerText = planetObj.name
+    p.dataset.id = `p${planetObj.id}`
 
     div.append(img, p)
 
+
     planetsCollection.append(div)
-
-    // showBuyModal(div)
-
+    planetDivsArray.push(div)
 }
-
-// function showBuyModal(div){
-
-//   // let modalDiv = document.querySelector('.modal')
-//   // let spanModal = document.querySelector('.close')
-//   // console.log(spanModal)
-
-
-// }
-
 
 // modal box creation
 function createBuyModalBox(planetsArray){
-
   let divPlanetBoxes = document.querySelectorAll('.planet-box')
-  console.log(divPlanetBoxes)
   
   // modalDiv is the entire box
-    let modalDiv = document.createElement('div')
-    modalDiv.id = "myModal"
-    modalDiv.className = "modal"
+  let modalDiv = document.createElement('div')
+  modalDiv.id = "buyModal"
+  modalDiv.className = "modal"
 
   //  content inside the box
-    let modalContent = document.createElement('div')
-    modalContent.className = "modal-content"  
+  let modalContent = document.createElement('div')
+  modalContent.className = "modal-content"  
 
   // creation of the close button of the box
-    let spanModal = document.createElement('span')
-    spanModal.className = "close"
-    spanModal.innerText = "x"
+  let spanModal = document.createElement('span')
+  spanModal.className = "close"
+  spanModal.innerText = "x"
 
-    let originDropDown = document.createElement('select')
-    originDropDown.className = "dropDown"
-    originDropDown.id = "origin-drop-down"
+  let originDropDown = document.createElement('select')
+  originDropDown.className = "dropDown"
+  originDropDown.id = "origin-drop-down"
 
-    let destinationDropDown = document.createElement('select')
-    destinationDropDown.className = "dropDown"
-    destinationDropDown.id = "destination-drop-down"
+  let destinationDropDown = document.createElement('select')
+  destinationDropDown.className = "dropDown"
+  destinationDropDown.id = "destination-drop-down"
 
-    planetsArray.forEach((planet) => 
-    {
-      let originOption = document.createElement('option')
-        originOption.value = planet.id
-        originOption.innerText = planet.name
-        originDropDown.append(originOption)
-      let destinationOption = document.createElement('option')
-        destinationOption.value = planet.id
-        destinationOption.innerText = planet.name
-        destinationDropDown.append(destinationOption)
-    })
-
-    // append drop down option to the dropdown list
-    // originDropDown.append(optionDropDown)
+  planetsArray.forEach((planet) => 
+  {
+    let originOption = document.createElement('option')
+      originOption.value = planet.id
+      originOption.innerText = planet.name
+      originDropDown.append(originOption)
+    let destinationOption = document.createElement('option')
+      destinationOption.value = planet.id
+      destinationOption.innerText = planet.name
+      destinationDropDown.append(destinationOption)
+  })
 
   //  append closing button to the modal content
-    modalContent.append(spanModal, originDropDown, destinationDropDown)
+  modalContent.append(spanModal, originDropDown, destinationDropDown)
 
   //  append modal content to the entire box
-    modalDiv.append(modalContent)
+  modalDiv.append(modalContent)
 
   //  append to the body of the html page
-    body.appendChild(modalDiv)
+  body.appendChild(modalDiv)
 
   spanModal.onclick = function() {
   modalDiv.style.display = "none";
   }
+
   window.onclick = function(event) {
     if (event.target == modalDiv) {
       modalDiv.style.display = "none";
     }
   }
-
-  // tell each planet box to render buymodal on click
-  divPlanetBoxes.forEach((box) => {
-    let clickedPlanetName = box.innerText
-    destinationDropDown
-    box.onclick = function(clickedPlanetName, destinationDropDown) {
-      console.log(destinationDropDown)
-      debugger
-      destinationDropDown.value = clickedPlanetName
-      modalDiv.style.display = "block";
-    }
+  
+  divPlanetBoxes.forEach(div => {
+    div.addEventListener("click", (e) => {
+      // debugger
+      let spanModal = document.querySelector("#buyModal")
+      spanModal.style.display = "block"
+      let destinationDropDown = document.querySelector("#destination-drop-down")
+      destinationDropDown.value = e.target.dataset.id[1]
+    })
   })
-
 }
-
-
-
